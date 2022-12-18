574a1139a0276b84efcedb427d35ea1f2da0d028
Maxim Gurin <mg@maximgurin.com>
2022-12-18 18:41:57 +0100

    Remove unnecessary helper methods from AclBuilder to have a more concise public API

--- lib/verifica/acl_builder.rb
+++ lib/verifica/acl_builder.rb
@@ -15,31 +15,6 @@ module Verifica
       self
     end
 
-    def allow_anonymous(actions)
-      allow(Sid.anonymous, actions)
-      self
-    end
-
-    def allow_authenticated(actions)
-      allow(Sid.authenticated, actions)
-      self
-    end
-
-    def allow_user(user_id, actions)
-      allow(Sid.user(user_id), actions)
-      self
-    end
-
-    def allow_organization(organization_id, actions)
-      allow(Sid.organization(organization_id), actions)
-      self
-    end
-
-    def allow_root(actions)
-      allow(Sid.root, actions)
-      self
-    end
-
     def build
       Acl.new(@aces)
     end
